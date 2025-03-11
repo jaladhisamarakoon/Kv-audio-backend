646fed25cd0bf4e3a3f80efdad61508983ecabd9
JaladhiSamarakoon <it22076120@my.sliit.lk>
2025-03-11 10:51:26 +0530

    implement the dotenv in the project

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ import mongoose  from "mongoose";
 import userRouter from "./routes/userRouter.js";
 import productRouter from "./routes/productRouter.js";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+
+
 
 
 
@@ -30,8 +37,7 @@ app.use((req,res,next)=>{
  next();
 })
 
-let mongoUrl = "mongodb+srv://admin:123@cluster0.wzkmc.mongodb.net/prods?retryWrites=true&w=majority&appName=Cluster0"
-
+let mongoUrl=process.env.MONGO_URL;
 mongoose.connect(mongoUrl)
 
 let connection = mongoose.connection
